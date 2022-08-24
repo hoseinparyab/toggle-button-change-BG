7b88ea09dc714a67b0240ad2b5d257bbfe69cb0f
hosein <hoseinparyab1@gmail.com>
2022-08-25 00:08:31 +0430

    add files

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+let checkbox = document.querySelector('.Switch__input');
+checkbox.addEventListener('change', function() {
+//change the theme of the website 
+document.body.classList.toggle('dark');
+
+});
